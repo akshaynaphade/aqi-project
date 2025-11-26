96e3f0eebe05c2d683ffe2107b54e758d315ee23
Akshay Naphade <81831871+akshaynaphade@users.noreply.github.com>
2025-11-26 12:05:06 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://your-backend-url.onrender.com'
+      '/api': 'https://aqi-project-r329.onrender.com/'
     }
   }
 })
